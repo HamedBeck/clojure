ea48ae95f504f2f469eb79660891ab4eeb402496
HamedBeck <hamed.beck@gmail.com>
2022-03-15 17:10:20 +0100

    first version of ts clojure

--- ts/src/ts/core.clj
+++ ts/src/ts/core.clj
@@ -11,67 +11,101 @@
 
 (def csv-data (ds/->dataset "city_data.csv"))
 
-(ds/head csv-data)
-(csv-data "Milan")
+#_(ds/head csv-data)
+#_(csv-data "Milan")
 
 
-(defn clean-trip
-  "..."
+#_(defn clean-trip
+  "create a map containing the cities"
   [trip]
   (let [trip (remove #(= % "Row-name") trip)]
     (zipmap (range 1 (+ (count trip) 1 )) trip)))
 
+;; generate secheduling rates
+(defn temp
+	"just genrate a vector of doubles
+	inputs:
+		N: number of temperature points
+                iniT : initial temerature
+		rate: decrase rate in temperature
+	output:
+		a list of doubles"
+
+	[N iniT rate]
+	 (for [i (range 1 N)] (/ iniT (Math/log (+ rate i)))))
 
 
-(defn helper
+(defn tempt
+  "..."
+  [iniT finT rate]
+  (range iniT finT rate))
+
+
+
+(defn init-trip
+  "Generate the first random configuration"
+  [csv-data]
+  (let [trip (remove #(= "Row-name" %) (ds/column-names csv-data))]
+    (zipmap (range 1 25) trip)))
+
+
+(defn update-trip
+  "update the trip map by swap of two random items"
+  [trip]
+  (let [i (+ 2 (rand-int (- (count trip) 2)))
+        j (+ 2 (rand-int (- (count trip) 2)))]
+    (-> trip (assoc i (trip j)) (assoc j (trip i)))))
+
+
+
+(defn pull-distance
+  "pulls distance value from matrix"
   [d-matrix s]
-  (let [s (first (sort s))]
-    (nth (d-matrix (val s)) (+ (key s) 1))))
+  (let [p (first (sort s))]
+    (nth (d-matrix (val p)) (key p))))
+
 
 (defn objective
   " calculate the objective value"
   [trip d-matrix]
-  (let [the-seq (seq trip)]
-    (loop [energy []
-           s the-seq]
-      (if (seq s)
-        (recur (conj energy (helper d-matrix s) )
-                   (rest s))
-        (/ (reduce + energy) 1000.0)))))
+  (loop [energy []
+         i 0]
+    (if (< i (- (count trip) 1))
+        (recur (conj energy (pull-distance d-matrix (filter #(> (key %) i) trip)))
+               (inc i))
+        (/ (reduce + energy) 1000.0))))
 
 
 
-
-
-(comment
-  (def trip (remove #(= "Row-name" %) (ds/column-names csv-data)))
-  (def map-trip (zipmap (range 1 25) trip))
-  (second (csv-data (val (first map-trip))))
-  (defn my-count
-  [col]
-  (let [the-seq (seq col)]
-    (loop [n 0 s the-seq]
-      (if (seq s)
-        (recur (inc n) (rest s))
-        n))))
-
-
-  )
+(defn acceptance
+  "compare to state and returen the best"
+  [c n t]
+  (let [cE (objective c csv-data)
+        nE (objective n csv-data)
+        delta (- nE cE)]
+      (cond (neg? delta)
+            c
+            (>  (Math/exp (/ (* delta -1) t)) (+ (rand 0.99) 0.01) )
+            n
+            :else
+            c)))
 
 
 
-#_(defn read-csv
-  [file-path]
-  (with-open [file (io/reader file-path)]
-    (doall (csv/read-csv file))))
+(defn metropolis
+  "..."
+  [csv-data temp]
+  (loop [best (init-trip csv-data)
+         i 0]
+    (if (< i (count temp))
+      (recur (acceptance best (update-trip best) (nth temp i))
+             (inc i)) best)))
 
-(defn csv-data->maps [csv-data]
-  (map zipmap
-       (->> (first csv-data) ;; First row is the header
-            (map keyword) ;; Drop if you want string keys instead
-            repeat)
-       (rest csv-data)))
-  #_(repeat (map keyword (first csv-data zipmap)))
+(defn runner
+  [csv-data iniT finT rate]
+  (let [temp (tempt iniT finT rate)]
+    (metropolis csv-data temp)))
+
 
 
 
@@ -79,20 +113,7 @@
 ;;(def csv-data (maps->tabular ))
 
 
-(def csv-data (csv-data->maps (read-csv "city_data.csv")))
-
-(defn temp
-	"just genrate a vector of doubles
-	inputs:
-		initN: initial temperature
-		finN: final temperature
-		rate: a negative value for decrase rate
-		in temperature
-	output:
-		a vector of doubles"
-
-	[initN finN rate]
-	(range initN finN rate))
+#_(def csv-data (csv-data->maps (read-csv "city_data.csv")))
 
 
 
